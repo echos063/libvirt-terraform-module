4722f8594020a63ccaa601c87990d626f4de1152
echos <gipnokot.echos@gmail.com>
2024-08-14 20:31:29 +0300

    Local Server Provisioning with Terraform without AWS/GCP

--- modules/networks/outputs.tf
+++ modules/networks/outputs.tf
@@ -2,6 +2,6 @@ output "linux_network_id" {
   value = libvirt_network.infra_network.id
 }
 
-output "backend_astra-linux_network_address" {
+output "backend_linux_network_address" {
   value = libvirt_network.infra_network.addresses[0]
 }
